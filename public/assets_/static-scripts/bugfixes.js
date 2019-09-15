09e06f1d9a2a951d5507ff9c6bdf0f9d8c7200b0
Robbert <robbertluit@hotmail.com>
2019-09-15 04:46:06 +0200

    overall fixes

--- public/assets_/static-scripts/bugfixes.js
+++ public/assets_/static-scripts/bugfixes.js
@@ -5,9 +5,11 @@ preventSettingMasonryGalleryAsAbsolute();
  * so with high number of images inside div - it won't scale anymore, It cannot be changed in twig
  * because JS overwrites it and besides i don't want to interfere with original code of that lib.
  */
-function preventSettingMasonryGalleryAsAbsolute(){
-    document.addEventListener("DOMContentLoaded", function() {
-        document.querySelector('.lightgallery .my-gallery').style = "";
-        document.querySelector('#aniimated-thumbnials').style     = "";
+function preventSettingMasonryGalleryAsAbsolute() {
+    document.addEventListener("DOMContentLoaded", function () {
+        if (document.querySelector('.lightgallery .my-gallery') != null) {
+            document.querySelector('.lightgallery .my-gallery').style = "";
+            document.querySelector('#aniimated-thumbnials').style = "";
+        }
     });
 }
